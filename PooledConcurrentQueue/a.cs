b1cf1a1e257fb844bdf68acc2bd2cbb56933378a
Molth Nevin <qq2201591870@163.com>
2025-07-29 16:03:50 +0800

    CACHE_LINE_SIZE

--- PooledConcurrentQueue/a.cs
+++ PooledConcurrentQueue/a.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 #pragma warning disable CA2208
 #pragma warning disable CS0169
 #pragma warning disable CS8602
 #pragma warning disable CS8632
+#pragma warning disable CS8762
 
 namespace AQueue
 {
@@ -13,12 +16,28 @@ namespace AQueue
     ///     ConcurrentQueue
     /// </summary>
     /// <typeparam name="T">Type</typeparam>
+    [StructLayout(LayoutKind.Sequential, Pack = ConcurrentQueue.CACHE_LINE_SIZE)]
     public sealed class PooledConcurrentQueue<T>
     {
         /// <summary>
-        ///     Handle
+        ///     Cross segment lock
         /// </summary>
-        private ConcurrentQueue.UnsafeConcurrentQueueHandle _handle;
+        private object _crossSegmentLock;
+
+        /// <summary>
+        ///     Segment pool
+        /// </summary>
+        private Stack<ConcurrentQueue.ConcurrentQueueSegmentArm64<T>> _segmentPool;
+
+        /// <summary>
+        ///     Tail
+        /// </summary>
+        private volatile ConcurrentQueue.ConcurrentQueueSegmentArm64<T> _tail;
+
+        /// <summary>
+        ///     Head
+        /// </summary>
+        private volatile ConcurrentQueue.ConcurrentQueueSegmentArm64<T> _head;
 
         /// <summary>
         ///     Structure
@@ -26,28 +45,11 @@ namespace AQueue
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public PooledConcurrentQueue()
         {
-            if (ArchitectureHelpers.NotArm64)
-                Unsafe.As<ConcurrentQueue.UnsafeConcurrentQueueHandle, ConcurrentQueue.ConcurrentQueueNotArm64<T>>(ref _handle) = new ConcurrentQueue.ConcurrentQueueNotArm64<T>();
-            else
-                Unsafe.As<ConcurrentQueue.UnsafeConcurrentQueueHandle, ConcurrentQueue.ConcurrentQueueArm64<T>>(ref _handle) = new ConcurrentQueue.ConcurrentQueueArm64<T>();
-        }
-
-        /// <summary>
-        ///     Not arm64
-        /// </summary>
-        private ConcurrentQueue.ConcurrentQueueNotArm64<T> NotArm64Handle
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => Unsafe.As<ConcurrentQueue.UnsafeConcurrentQueueHandle, ConcurrentQueue.ConcurrentQueueNotArm64<T>>(ref _handle);
-        }
-
-        /// <summary>
-        ///     Arm64
-        /// </summary>
-        private ConcurrentQueue.ConcurrentQueueArm64<T> Arm64Handle
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => Unsafe.As<ConcurrentQueue.UnsafeConcurrentQueueHandle, ConcurrentQueue.ConcurrentQueueArm64<T>>(ref _handle);
+            _crossSegmentLock = new object();
+            _segmentPool = new Stack<ConcurrentQueue.ConcurrentQueueSegmentArm64<T>>();
+            var segment = new ConcurrentQueue.ConcurrentQueueSegmentArm64<T>();
+            segment.Initialize();
+            _tail = _head = segment;
         }
 
         /// <summary>
@@ -56,7 +58,22 @@ namespace AQueue
         public bool IsEmpty
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => ArchitectureHelpers.NotArm64 ? NotArm64Handle.IsEmpty : Arm64Handle.IsEmpty;
+            get
+            {
+                var segment = _head;
+                while (true)
+                {
+                    var next = Volatile.Read(ref segment.NextSegment);
+                    if (segment.TryPeek())
+                        return false;
+                    if (next != null)
+                        segment = next;
+                    else if (Volatile.Read(ref segment.NextSegment) == null)
+                        break;
+                }
+
+                return true;
+            }
         }
 
         /// <summary>
@@ -65,7 +82,49 @@ namespace AQueue
         public int Count
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => ArchitectureHelpers.NotArm64 ? NotArm64Handle.Count : Arm64Handle.Count;
+            get
+            {
+                var spinWait = new NativeSpinWait();
+                while (true)
+                {
+                    var head = _head;
+                    var tail = _tail;
+                    var headHead = Volatile.Read(ref head.HeadAndTail.Head);
+                    var headTail = Volatile.Read(ref head.HeadAndTail.Tail);
+                    if (head == tail)
+                    {
+                        if (head == _head && tail == _tail && headHead == Volatile.Read(ref head.HeadAndTail.Head) && headTail == Volatile.Read(ref head.HeadAndTail.Tail))
+                            return GetCount(headHead, headTail);
+                    }
+                    else if (head.NextSegment! == tail)
+                    {
+                        var tailHead = Volatile.Read(ref tail.HeadAndTail.Head);
+                        var tailTail = Volatile.Read(ref tail.HeadAndTail.Tail);
+                        if (head == _head && tail == _tail && headHead == Volatile.Read(ref head.HeadAndTail.Head) && headTail == Volatile.Read(ref head.HeadAndTail.Tail) && tailHead == Volatile.Read(ref tail.HeadAndTail.Head) && tailTail == Volatile.Read(ref tail.HeadAndTail.Tail))
+                            return GetCount(headHead, headTail) + GetCount(tailHead, tailTail);
+                    }
+                    else
+                    {
+                        lock (_crossSegmentLock)
+                        {
+                            if (head == _head && tail == _tail)
+                            {
+                                var tailHead = Volatile.Read(ref tail.HeadAndTail.Head);
+                                var tailTail = Volatile.Read(ref tail.HeadAndTail.Tail);
+                                if (headHead == Volatile.Read(ref head.HeadAndTail.Head) && headTail == Volatile.Read(ref head.HeadAndTail.Tail) && tailHead == Volatile.Read(ref tail.HeadAndTail.Head) && tailTail == Volatile.Read(ref tail.HeadAndTail.Tail))
+                                {
+                                    var count = GetCount(headHead, headTail) + GetCount(tailHead, tailTail);
+                                    for (var s = head.NextSegment!; s != tail; s = s.NextSegment!)
+                                        count += s.HeadAndTail.Tail - ConcurrentQueue.FREEZE_OFFSET;
+                                    return count;
+                                }
+                            }
+                        }
+                    }
+
+                    spinWait.SpinOnce(-1);
+                }
+            }
         }
 
         /// <summary>
@@ -74,10 +133,22 @@ namespace AQueue
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Clear()
         {
-            if (ArchitectureHelpers.NotArm64)
-                NotArm64Handle.Clear();
-            else
-                Arm64Handle.Clear();
+            lock (_crossSegmentLock)
+            {
+                _tail.EnsureFrozenForEnqueues();
+                var node = _head;
+                while (node != null)
+                {
+                    var temp = node;
+                    node = node.NextSegment!;
+                    _segmentPool.Push(temp);
+                }
+
+                if (!_segmentPool.TryPop(out var segment))
+                    segment = new ConcurrentQueue.ConcurrentQueueSegmentArm64<T>();
+                segment.Initialize();
+                _tail = _head = segment;
+            }
         }
 
         /// <summary>
@@ -87,10 +158,27 @@ namespace AQueue
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Enqueue(in T item)
         {
-            if (ArchitectureHelpers.NotArm64)
-                NotArm64Handle.Enqueue(item);
-            else
-                Arm64Handle.Enqueue(item);
+            if (!_tail.TryEnqueue(item))
+            {
+                while (true)
+                {
+                    var tail = _tail;
+                    if (tail.TryEnqueue(item))
+                        return;
+                    lock (_crossSegmentLock)
+                    {
+                        if (tail == _tail)
+                        {
+                            tail.EnsureFrozenForEnqueues();
+                            if (!_segmentPool.TryPop(out var newTail))
+                                newTail = new ConcurrentQueue.ConcurrentQueueSegmentArm64<T>();
+                            newTail.Initialize();
+                            tail.NextSegment = newTail;
+                            _tail = newTail;
+                        }
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -99,38 +187,58 @@ namespace AQueue
         /// <param name="result">Item</param>
         /// <returns>Dequeued</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool TryDequeue([NotNullWhen(true)] out T? result) => ArchitectureHelpers.NotArm64 ? NotArm64Handle.TryDequeue(out result) : Arm64Handle.TryDequeue(out result);
-    }
-
-    /// <summary>
-    ///     ConcurrentQueue
-    /// </summary>
-    internal static partial class ConcurrentQueue
-    {
-        /// <summary>
-        ///     ConcurrentQueue
-        /// </summary>
-        public struct UnsafeConcurrentQueueHandle
+        public bool TryDequeue([NotNullWhen(true)] out T? result)
         {
-            /// <summary>
-            ///     Cross segment lock
-            /// </summary>
-            private object _crossSegmentLock;
+            var head = _head;
+            if (head.TryDequeue(out result))
+                return true;
+            if (head.NextSegment == null)
+            {
+                result = default;
+                return false;
+            }
 
-            /// <summary>
-            ///     Segment pool
-            /// </summary>
-            private Stack<object> _segmentPool;
+            while (true)
+            {
+                head = _head;
+                if (head.TryDequeue(out result))
+                    return true;
+                if (head.NextSegment == null)
+                {
+                    result = default;
+                    return false;
+                }
 
-            /// <summary>
-            ///     Tail
-            /// </summary>
-            private volatile object? _tail;
+                if (head.TryDequeue(out result))
+                    return true;
+                lock (_crossSegmentLock)
+                {
+                    if (head == _head)
+                    {
+                        _head = head.NextSegment;
+                        _segmentPool.Push(head);
+                    }
+                }
+            }
+        }
 
-            /// <summary>
-            ///     Head
-            /// </summary>
-            private volatile object? _head;
+        /// <summary>
+        ///     Get count
+        /// </summary>
+        /// <param name="head">Head</param>
+        /// <param name="tail">Tail</param>
+        /// <returns>Count</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int GetCount(int head, int tail)
+        {
+            if (head != tail && head != tail - ConcurrentQueue.FREEZE_OFFSET)
+            {
+                head &= ConcurrentQueue.SLOTS_MASK;
+                tail &= ConcurrentQueue.SLOTS_MASK;
+                return head < tail ? tail - head : ConcurrentQueue.SLOTS_LENGTH - head + tail;
+            }
+
+            return 0;
         }
     }
 }
